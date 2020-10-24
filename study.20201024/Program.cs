5fab9fe66e3d3c8bb850eb2449758628ebd4ec01
takuma-ishino <takuma2566@icloud.com>
2020-10-24 11:10:05 +0900

    今日の課題

---
+++ study.20201024/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace study._20201024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
